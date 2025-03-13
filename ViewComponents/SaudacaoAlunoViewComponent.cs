f3a82d416247d567286392f7d0878aa5ea16f60b
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-12 22:25:09 -0300

    View e Razor: Conhecendo os tagHelpers, configurações de views, partial viewse e componentes.

---
+++ ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Henrick",
+                Email = ""
+            };
+
+            return View(aluno);
+        }
+    }
+}

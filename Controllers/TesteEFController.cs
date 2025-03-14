bf69f4fe536efefb921541fd1c1169e9d5148d18
Henrick Adrian <henrick.adrian4@gmail.com>
2025-03-13 23:55:11 -0300

    Operações no banco com EF.

---
+++ Controllers/TesteEFController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class TesteEFController : Controller
+    {
+        public AppDbContext Db { get; set; }
+
+        public TesteEFController(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Henrick",
+                Email = "henrick@email.com",
+                DataNascimento = new DateTime(2001, 1, 1),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            ////----criando o aluno
+            //Db.Alunos.Add(aluno);
+            //Db.SaveChanges();
+
+            ////----obtendo os dados dele
+            var alunoChange = Db.Alunos.Where(a => a.Nome.Contains("Henrick")).FirstOrDefault();
+            //alunoChange.Nome = "Henrick Adrian";
+
+            ////Como salvo
+            //Db.Alunos.Update(alunoChange);
+            //Db.SaveChanges();
+
+
+            Db.Alunos.Remove(alunoChange);
+            Db.SaveChanges();
+
+            return Content("");
+        }
+    }
+}
